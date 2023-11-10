8ec0c73fc8d68119fc601770dd400a6b8a2d2b64
blueT <lionel32204@gmail.com>
2023-11-10 18:49:16 +0700

    Fix ver 2

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
      public static void main(String[] args) {
-         Login.chooseModeLogin();
+        Login.chooseModeLogin();
      }
 }
